59eb3d42b8f3ee78ac9c0f3f12a7d35f80fe0aa1
aioloto <programmerdresscode@gmail.com>
2015-11-07 03:18:26 -0500

    cname

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,6 +1,8 @@
 require "rubygems"
 require "bundler/setup"
-require "stringex"
+# require "stringex"
+# Rquire jekyll to compile the site.
+require "jekyll"
 
 ## -- Config -- ##
 
@@ -73,6 +75,59 @@ task :new_page, :title do |t, args|
     page.puts "---"
   end
 end
+
+
+# Github pages publishing.
+
+  # Because we are using 3rd party plugins for jekyll to manage the asset pipeline
+  # and suchlike we are unable to just branch the code, we have to process the site
+  # localy before pushing it to the branch to publish.
+  #
+  # We built this little rake task to help make that a little bit eaiser.
+  #
+
+  # Usaage:
+  # bundle exec rake blog:publish
+  desc "Publish blog to gh-pages"
+  task :publish do
+    # Compile the Jekyll site using the config.
+    Jekyll::Site.new(Jekyll.configuration({
+      "source"      => ".",
+      "destination" => "_site",
+      "config" => "_config_prod.yml"
+    })).process
+
+    # Get the origin to which we are going to push the site.
+    origin = `git config --get remote.origin.url`
+
+    # Make a temporary directory for the build before production release.
+    # This will be torn down once the task is complete.
+    Dir.mktmpdir do |tmp|
+      # Copy accross our compiled _site directory.
+      cp_r "_site/.", tmp
+
+      # Switch in to the tmp dir.
+      Dir.chdir tmp
+
+      # Prepare all the content in the repo for deployment.
+      system "touch .nojekyll"
+
+      system "git init" # Init the repo.
+      system "git config user.name 'aioloto'"
+      system "git config user.email 'programmerdresscode@gmail.com'"
+      system "git config credential.https://github.com.username aioloto"
+      system "git add . && git commit -m 'Site updated at #{Time.now.utc}'" # Add and commit all the files.
+
+      # Add the origin remote for the parent repo to the tmp folder.
+      system "git remote add origin #{origin}"
+
+      # Push the files to the gh-pages branch, forcing an overwrite.
+      system "git push origin master:refs/heads/gh-pages --force"
+    end
+
+    # Done.
+  end
+
 
 def get_stdin(message)
   print message
